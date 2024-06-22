f9bc13335062e2992d71cb2b1ab4dceb03a460f5
Alonso del Arte <alonso.delarte@gmail.com>
2024-06-21 22:45:58 -0400

    New class

---
+++ test/textops/json/JSONValidatorTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package textops.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Tests of the JSONValidator class.
+ * @author Alonso del Arte
+ */
+public class JSONValidatorTest {
+    
+    /**
+     * Test of main method, of class JSONValidator.
+     */
+//    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        JSONValidator.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
