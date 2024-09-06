b1199ce4e9cf0f4169f492c098098d8315728f87
Alonso del Arte <alonso.delarte@gmail.com>
2024-09-05 22:31:11 -0400

    Preparing some imports

--- src/textops/json/JSONMinifier.java
+++ src/textops/json/JSONMinifier.java
@@ -1,13 +1,33 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package textops.json;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  *
- * @author al
+ * @author Alonso del Arte
  */
 public class JSONMinifier {
+    
+    public static void main(String[] args) {
+        //
+    }
     
 }
